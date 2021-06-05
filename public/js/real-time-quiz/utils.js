d3b241db01d3a1234437bb13945a61112bbee0c4
Luis Manzanero <manzanero.luis1995@gmail.com>
2021-06-06 00:22:53 +0800

    fix script linking for other js file

--- public/js/real-time-quiz/utils.js
+++ public/js/real-time-quiz/utils.js
@@ -1,4 +1,4 @@
-export const isAnswerSaidCorrectly = (question, answer) => {  
+ const isAnswerSaidCorrectly = (question, answer) => {  
     console.log(question.length, answer.length + 1);
     if(question.toLowerCase().includes(answer.toLowerCase()) && answer.length + 1 == question.length) {  
       return true;
